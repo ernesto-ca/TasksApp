e6f5a42f1773d47929d963d06f81b1b5c9de1a4b
ernesto-ca <ernestocabanasdev@gmail.com>
2024-05-30 22:37:51 -0600

    Using a new theme and logo!

--- app/src/main/java/ec/project/todos/ui/theme/Color.kt
+++ app/src/main/java/ec/project/todos/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package ec.project.todos.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue = Color(0xFF03A9F4)
+val Ocean = Color(0xFF2196F3)
+val Sky = Color(0xFF00BCD4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBlue = Color(0xFF3F51B5)
+val DarkOcean = Color(0xFF2E3A80)
+val Night = Color(0xFF04167A)
--- app/src/main/java/ec/project/todos/ui/theme/Theme.kt
+++ app/src/main/java/ec/project/todos/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue,
+    secondary = Ocean,
+    tertiary = Sky
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = DarkBlue,
+    secondary = DarkOcean,
+    tertiary = Night
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
